82ee6f568a9c9f6183efa5098d6621570ebbd785
keyrun-14 <tamvadakirankumar007gmail.com>
2022-03-06 10:06:39 +0530

    create_objects submission

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,18 +1,18 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
-    if(hasDrivingLiscence==false)
+    if(hasDrivingLiscence===false)
     {
         return "You cannot drive"
     }
-    else if(isTired==false && isSober==false)
+    else if(isTired===true)
     {
         return "You shouldn't drive"
     }
-    else if(isTired==true && isSober==false)
+    else if(isSober===false)
     {
         return "You shouldn't drive"
     }
-    else if(isTired==false  && isSober==true)
+    else if(isTired===false  && isSober===true)
     {
         return  "You can drive"
     }
